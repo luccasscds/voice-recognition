99bf9d870ca21affcb0b3d451ebd352d0d6f1e2d
Lucas da silva costa <lucas@unicorp.com.br>
2023-03-17 23:16:22 -0300

    configurando o deploy

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 var_production='production'
 var_url_origin=$(git remote get-url origin)
-
-set -e
+# git branch --show-current
 
 npm run build
-npm version --patch
+npm version patch
 
 # Git
 git push origin --tags
+git push origin HEAD
 
 cd dist/
 # Git
 git init
-git checkout -B $var_production
+git checkout -b $var_production
 git add -A
 git commit -m '🚀 Deploy'
 # git commit --amend -m '🚀 Deploy'
 git push -f $var_url_origin $var_production
 
-cd -
+cd ..
