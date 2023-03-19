6ff180a37d609b0a651b41eaa894e870852cf950
Lucas da silva costa <lucas@unicorp.com.br>
2023-03-19 01:36:54 -0300

    configurando file deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-var_production='production'
+var_branch_production='production'
 var_url_origin=$(git remote get-url origin)
 # git branch --show-current
 
 npm run build
 npm version patch
+var_version=`node -e "console.log(require('./package.json').version)"`
+git commit --amend -m 'v'$var_version
 
 # Git
 git push origin --tags
@@ -12,10 +14,9 @@ git push origin HEAD
 cd dist/
 # Git
 git init
-git checkout -b $var_production
+git checkout -b $var_branch_production
 git add -A
 git commit -m '🚀 Deploy'
-# git commit --amend -m '🚀 Deploy'
-git push -f $var_url_origin $var_production
+git push -f $var_url_origin $var_branch_production
 
 cd ..
